5d171772f6ff502cdf493a04e09cdb053a6ba731
italo jose <italo.i@live.com>
2025-03-14 15:48:36 -0300

    wip

---
+++ server/tokenizer.js
@@ -0,0 +1,18 @@
+import { Tokenizer } from "@anush008/tokenizers";
+
+
+const cwd = process.cwd().replace('/local/build/programs/server', '/local/build/programs/web.browser/app/tokenizer.json')
+const tokenizer = await Tokenizer.fromFile(cwd);
+const wpEncoded = await tokenizer.encode("Who is John?");
+
+console.log(wpEncoded.getLength());
+console.log(wpEncoded.getTokens());
+console.log(wpEncoded.getIds());
+console.log(wpEncoded.getAttentionMask());
+console.log(wpEncoded.getOffsets());
+console.log(wpEncoded.getOverflowing());
+console.log(wpEncoded.getSpecialTokensMask());
+console.log(wpEncoded.getTypeIds());
+console.log(wpEncoded.getWordIds());
+
+export default tokenizer; 
