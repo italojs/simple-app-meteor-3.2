98b515a4dc097896c6d4257c3f274a9bdce87aac
italo jose <italo.i@live.com>
2025-03-14 16:21:19 -0300

    wip

--- tests/main.js
+++ tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("meteor-app", function () {
+describe("app", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "meteor-app");
+    assert.strictEqual(name, "app");
   });
 
   if (Meteor.isClient) {
